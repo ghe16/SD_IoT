db254dfb67ad1679a76a76135ab436412ebc65c8
Gustavo Adolfo Hernandez Penaloza <137998587+ghe16@users.noreply.github.com>
2024-10-29 16:59:32 +0100

    Update SimpleProducer.java

--- Kafka/producer_consumer/src/main/java/com/kafka/example/SimpleProducer.java
+++ Kafka/producer_consumer/src/main/java/com/kafka/example/SimpleProducer.java
@@ -1,51 +1,55 @@
-package com.kafka.example;
+package kafka.example1;
+
 import java.util.Properties;
 
 import org.apache.kafka.clients.producer.KafkaProducer;
 import org.apache.kafka.clients.producer.ProducerRecord;
 
 public class SimpleProducer {
-    Properties props;   //  the producer is a kafka client that publishes messages to the kafka cluster
-    public static int numberofMessages = 200;
-    KafkaProducer <String, String> producer;
 
-    //********************************************************
-    // 1 KAFKA Producer configuration
-    //*************************************************
+	Properties props; // The producer is a Kafka client that publishes records to the Kafka cluster.
 
-    SimpleProducer(){
-        Properties props = new Properties();
-        props.put("bootstrap.servers", "localhost:9092");
-        props.put("acks","all");
-        // Serializer for converting the key type to bytes
-        props.put("key.serializer","org.apache.kafka.common.serialization.StringSerializer");
-        //Serializer for converting the value type to bytes
-        props.put("value.serializer","org.apache.kafka.common.serialization.StringSerializer");
+	KafkaProducer<String, String> producer;
+	
+//****************************************************************
+// 1.KAFKA PRODUCER CONFIGURATION 
+//****************************************************************
+	SimpleProducer() {
+		
+		Properties props = new Properties();
+		props.put("bootstrap.servers", "localhost:9092");
+		props.put("acks", "all");
+		// Serializer for conversion the key type to bytes
+		props.put("key.serializer",
+				"org.apache.kafka.common.serialization.StringSerializer");
+		// Serializer for conversion the value type to bytes
+		props.put("value.serializer",
+				"org.apache.kafka.common.serialization.StringSerializer");
 
-        producer = new KafkaProducer <String, String>(props);
-    }
+		producer = new KafkaProducer<String, String>(props);
+	}
+	
+//****************************************************************
+//2. TOPIC PRODUCTION
+//****************************************************************	
+	void produceAndPrint() {
+		for (int i = 0; i < 200; i++)
+			// Fire-and-forget send(topic, key, value)
+			// Send adds records to unsent records buffer and return
+			producer.send(new ProducerRecord<String, String>("DS", Integer
+					.toString(i), Integer.toString(i)));
 
-    //********************************************************
-    // 2 Topic Production
-    //*************************************************
+	}
 
-    void prodcueAndPrint(){
-        for (int i = 0; i < numberofMessages; i++)
-        // Fire and forget send(topic, key, value)
-        // Send addss records to unsent records buffer and return
-        producer.send(new ProduceRecord<String, String>("DS", Integer.toString(i), Integer.toString(i)));
+	void stop() {
+		producer.close();
+	}
 
-    }
+	public static void main(String[] args) {
+		//System.setProperty("kafka.logs.dir", "/home/isabel/kafka/logs");
+		SimpleProducer myProducer = new SimpleProducer();
+		myProducer.produceAndPrint();
+		myProducer.stop();
 
-    void stop() {
-        producer.close();
-    }
-
-
-    public static void main(String[] args) {
-        SimpleProducer myProducer = new SimpleProducer();
-        myProducer.prodcueAndPrint();
-        myProducer.stop();
-    }
-
+	}
 }
